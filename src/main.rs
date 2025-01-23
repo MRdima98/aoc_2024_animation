32f9782c2dd66d151f59dd43cb08d798353b508c
dima <dimafrunza69@gmail.com>
2025-01-23 08:23:19 +0100

    Tailwind

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,8 @@ fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button
-            on:click=move |_| *set_count.write() +=1
-        >
-            "Click me: "
-            {count}
-        </button>
-        <p>
-            "Double count: "
-            {move || count.get() * 2}
-        </p>
+        <div>Square</div>
+        <button on:click=move |_| *set_count.write() += 1>"Click me: " {count}</button>
+        <p>"Double count: " {move || count.get() * 2}</p>
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBg: "#416A65",
+        secondaryBg: "#576C5F",
+        text: "#EEEEEE",
+      },
+    },
+  },
+  plugins: [],
+};

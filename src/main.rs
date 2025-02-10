e6c8ad5279908b52eb082f2c20cf265aa4577860
dima <dimafrunza69@gmail.com>
2025-02-10 07:25:15 +0100

    Correct heights

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use gloo_timers::future::TimeoutFuture;
 use leptos::mount::mount_to_body;
 use leptos::prelude::*;
 use leptos::task::spawn_local;
+use leptos_use::use_window_size;
 
 fn main() {
     mount_to_body(App);
@@ -9,29 +10,38 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    //let (right, set_right) = signal("blue".to_string());
-    //let mut values = vec![100, 200, 300];
-    let (values, set_values) = signal(vec![100]);
-    let (width, _) = signal(vec![200, 300, 400, 500]);
+    let screen = use_window_size();
+    let (values, set_values) = signal(vec![]);
+    let mut width = vec![200, 300, 400, 500];
+    let longest = width.iter().max().unwrap().clone();
+    let height = ((screen.height.get() as i32) * 11/12) /width.iter().len() as i32;
+    let mut lengths = vec![];
+
+    for el in width.iter_mut() {
+        if *el == longest {
+            *el = 100 as i32;
+            continue;
+        } 
+        *el = (*el * 100) / longest;
+    }
+
+    for el in width.iter() {
+        lengths.push((*el, height));
+    }
+
+    let (width, _) = signal(lengths);
 
     spawn_local(async move{
         for el in width.get() {
-            //sleep(Duration::from_millis(200)).await;
-            TimeoutFuture::new(500).await; // Wait 200ms
+            TimeoutFuture::new(500).await;
             set_values.write().push(el);
-            //Timeout::new(1000, move || {
-            //    //*set_color.write() = "blue".to_string();
-            //    //*set_pos.write() = "20px"
-            //    set_values.write().push(el);
-            //})
-            //.forget();
         }
     });
 
 
 
     view! {
-        <ul>
+        <ul class="w-11/12 h-11/12">
             <For
                 each=move || values.get()
                 key=|item| item.clone()
@@ -39,7 +49,7 @@ fn App() -> impl IntoView {
                     view! {
                         <li
                             class="bg-violet-500"
-                            style=format!("width: {}px; height: {}px", item, 10)
+                            style=format!("width: {}%; height: {}px", item.0, item.1)
                         ></li>
                     }
                 }

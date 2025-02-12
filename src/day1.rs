affae007a9b1b27871a5103f708169cfcaa721a7
dima <dimafrunza69@gmail.com>
2025-02-12 08:57:25 +0100

    progress

---
+++ src/day1.rs
@@ -0,0 +1,102 @@
+use gloo_timers::future::TimeoutFuture;
+use leptos::prelude::*;
+use leptos::task::spawn_local;
+use leptos_use::use_window_size;
+
+#[component]
+pub fn Day1() -> impl IntoView {
+let screen = use_window_size();
+    let (values, set_values) = signal(vec![]);
+    let (left, setLeft) = signal(vec![]);
+    let mut right = vec![];
+    let mut diff = vec![];
+    spawn_local(async move{
+        (*setLeft.write(), right, diff) = get_todo().await.unwrap();
+    });
+    
+    let max1 = left.get().iter().max().unwrap().clone();
+    let max2 = left.iter().max().unwrap().clone();
+    let max = max1.max(max2);
+    let height = ((screen.height.get() as usize) * 11/12) /left.iter().len() as usize;
+    let mut lists = vec![];
+
+    for el in left.get().iter_mut() {
+        if *el == max {
+            *el = 100 as usize;
+            continue;
+        } 
+        *el = (*el * 100) / max;
+    }
+
+    for el in right.iter_mut() {
+        if *el == max {
+            *el = 100 as usize;
+            continue;
+        } 
+        *el = (*el * 100) / max;
+    }
+
+    let mut i = 0;
+    for el in left.iter() {
+        lists.push((*el, right[i],height ));
+        i += 1;
+    }
+
+    let (width, _) = signal(lists);
+    let (text, setText) = signal(" empty".to_string());
+
+    spawn_local(async move{
+        for el in width.get() {
+            TimeoutFuture::new(200).await;
+            set_values.write().push(el);
+        }
+    });
+
+    view! {
+        <div class="text-white">Num: {text}</div>
+        <ul class="w-11/12 h-11/12">
+            <For
+                each=move || values.get()
+                key=|item| item.clone()
+                children=|item| {
+                    view! {
+                        <li class="relative">
+                            <div
+                                class="bg-green-500 absolute"
+                                style=format!("width: {}%; height: {}px", item.0, item.2)
+                            ></div>
+                            <div
+                                class="bg-violet-500"
+                                style=format!("width: {}%; height: {}px", item.1, item.2)
+                            ></div>
+                        </li>
+                    }
+                }
+            />
+        </ul>
+    }
+}
+
+#[server(GetTodo, "/api")]
+pub async fn get_todo() -> Result<(Vec<usize>, Vec<usize>, Vec<usize>), ServerFnError> {
+    let mut left = vec![];
+    let mut right = vec![];
+    let mut diff = vec![];
+
+    let left_file = include_str!("../data/day1_left");
+    for el in left_file.lines() {
+        left.push(el.parse::<usize>().unwrap());
+    }
+
+    let right_file = include_str!("../data/day1_right");
+    for el in right_file.lines() {
+        right.push(el.parse::<usize>().unwrap());
+    }
+
+    let diff_file = include_str!("../data/day1_diff");
+    for el in diff_file.lines() {
+        diff.push(el.parse::<usize>().unwrap());
+    }
+
+    Ok((left, right, diff))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use day1::Day1;
 use gloo_timers::future::TimeoutFuture;
 use leptos::mount::mount_to_body;
 use leptos::prelude::*;
 use leptos_router::{components::*, path};
 use leptos::task::spawn_local;
 use leptos_use::use_window_size;
+
+pub mod day1;
 
 fn main() {
     mount_to_body(App);
@@ -15,7 +18,7 @@ fn App() -> impl IntoView {
         <Router>
             <Routes fallback=|| "Not found.">
                 <Route path=path!("/") view=Day1 />
-                <Route path=path!("/users") view=Users />
+            // <Route path=path!("/users") view=Users />
             </Routes>
         </Router>
     }
@@ -26,78 +29,3 @@ fn Users() -> impl IntoView {
     view! { "hello" }
 }
 
-#[component]
-fn Day1() -> impl IntoView {
-let screen = use_window_size();
-    let (values, set_values) = signal(vec![]);
-    let mut list1 = vec![200, 300, 400, 500];
-    let mut list2 = vec![210, 250, 450, 470];
-    let max1 = list1.iter().max().unwrap().clone();
-    let max2 = list1.iter().max().unwrap().clone();
-    let max = max1.max(max2);
-    let height = ((screen.height.get() as i32) * 11/12) /list1.iter().len() as i32;
-    let mut lists = vec![];
-
-    for el in list1.iter_mut() {
-        if *el == max {
-            *el = 100 as i32;
-            continue;
-        } 
-        *el = (*el * 100) / max;
-    }
-
-    for el in list2.iter_mut() {
-        if *el == max {
-            *el = 100 as i32;
-            continue;
-        } 
-        *el = (*el * 100) / max;
-    }
-
-    let mut i = 0;
-    for el in list1.iter() {
-        lists.push((*el, list2[i],height ));
-        i += 1;
-    }
-
-    let (width, _) = signal(lists);
-    let (text, setText) = signal(" empty".to_string());
-
-    spawn_local(async move{
-        for el in width.get() {
-            TimeoutFuture::new(200).await;
-            set_values.write().push(el);
-        }
-
-        *setText.write() = get_todo().await.unwrap();
-    });
-
-    view! {
-        <div class="text-white">Num: {text}</div>
-        <ul class="w-11/12 h-11/12">
-            <For
-                each=move || values.get()
-                key=|item| item.clone()
-                children=|item| {
-                    view! {
-                        <li class="relative">
-                            <div
-                                class="bg-green-500 absolute"
-                                style=format!("width: {}%; height: {}px", item.0, item.2)
-                            ></div>
-                            <div
-                                class="bg-violet-500"
-                                style=format!("width: {}%; height: {}px", item.1, item.2)
-                            ></div>
-                        </li>
-                    }
-                }
-            />
-        </ul>
-    }
-}
-
-#[server(GetTodo, "/api")]
-pub async fn get_todo() -> Result<String, ServerFnError> {
-    Ok("five".to_string())
-}
